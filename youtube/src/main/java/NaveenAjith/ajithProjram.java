9f66b84c73fcd16202071d5d167c8515be340040
NaveenRayappan1808 <88507288+NaveenRayappan1808@users.noreply.github.com>
2024-08-31 20:32:35 +0530

    added daughter details.java
    Added daughter details

--- youtube/src/main/java/NaveenAjith/ajithProjram.java
+++ youtube/src/main/java/NaveenAjith/ajithProjram.java
@@ -5,6 +5,7 @@ public class ajithProjram {
 	public static void main(String[] args) {
 		System.out.println("Ajith kumar");
 		System.out.println("Shalini");
+		System.out.println("Aushya");
 
 	}
 
